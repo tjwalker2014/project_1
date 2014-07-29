f667bc99d46c7d6320300f0943eb6c8e1f17a4d0
Tom <tj.walker@live.com>
2014-07-29 10:37:59 +0100

    Tuesday morning, added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 # Courses
 # Instructors
 # Admin user?
+
+Campus.delete_all
+Room.delete_all
+Course.delete_all
+User.delete_all
+
+campus1 = Campus.create(name: "GA London", location: "London", google_map_source: "googlesourcecodehere")
+campus2 = Campus.create(name: "GA New York", location: "New York", google_map_source: "googlesourcecodehere")
+campus3 = Campus.create(name: "GA Sydney", location: "Sydney", google_map_source: "googlesourcecodehere")
+
+room1 = Room.create(name: "Room 1", campus_id: campus1.id)
+room2 = Room.create(name: "Room 2", campus_id: campus2.id)
+room3 = Room.create(name: "Room 3", campus_id: campus3.id)
+
+course1 = Course.create(name: "Web Development Immersive", description: "blah blah blah", room_id: room1.id, start_detail: DateTime.new(2014, 8, 4), end_detail: DateTime.new(2014, 8, 4))
+course2 = Course.create(name: "User Experience Design Immersive", description: "blah blah blah", room_id: room2.id, start_detail: DateTime.new(2014, 8, 4), end_detail: DateTime.new(2014, 8, 4))
+course3 = Course.create(name: "Product Management", description: "blah blah blah", room_id: room3.id, start_detail: DateTime.new(2014, 8, 4), end_detail: DateTime.new(2014, 8, 4))
+
+user1 = User.create(name: "Tom", course_id: course1.id)
+user2 = User.create(name: "David", course_id: course2.id)
+user3 = User.create(name: "Gwen", course_id: course3.id)
